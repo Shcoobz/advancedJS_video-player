500d80b01d72bc102b912932eddfbf580806410d
Shcoobz <a.boeckmann23@gmail.com>
2024-05-16 15:35:59 +0200

    fixed default player speed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import PlayControls from './controls/PlayControls';
 import VolumeControls from './controls/VolumeControls';
--- src/components/controls/SpeedControls.jsx
+++ src/components/controls/SpeedControls.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 /**
  * SpeedControls provides a dropdown select element for users to change the playback speed of media.
@@ -6,14 +7,36 @@ import PropTypes from 'prop-types';
  * @param {Function} onSpeedChange - Callback function to be invoked when the playback speed is changed via the dropdown.
  */
 function SpeedControls({ onSpeedChange }) {
+  const [selectedSpeed, setSelectedSpeed] = useState('1');
+
+  /**
+   * React effect that updates the video playback speed whenever the `selectedSpeed` state or `onSpeedChange` callback changes.
+   * It effectively simulates a change event to adjust the playback speed based on the selected option.
+   */
+  useEffect(() => {
+    onSpeedChange({ target: { value: selectedSpeed } });
+  }, [onSpeedChange, selectedSpeed]);
+
+  /**
+   * Handles changes to the speed selection dropdown.
+   * Sets the new speed using the `setSelectedSpeed` state updater function and invokes the `onSpeedChange` callback.
+   * @param {Event} e - The event object that includes the newly selected speed value.
+   */
+  function handleSpeedChange(e) {
+    setSelectedSpeed(e.target.value);
+    onSpeedChange(e);
+  }
+
   return (
     <div className='speed' title='Playback Rate'>
-      <select name='playbackRate' className='player-speed' onChange={onSpeedChange}>
+      <select
+        name='playbackRate'
+        className='player-speed'
+        onChange={handleSpeedChange}
+        value={selectedSpeed}>
         <option value='0.5'>0.5x</option>
         <option value='0.75'>0.75x</option>
-        <option value='1' defaultValue>
-          1x
-        </option>
+        <option value='1'>1x</option>
         <option value='1.5'>1.5x</option>
         <option value='2'>2x</option>
       </select>
